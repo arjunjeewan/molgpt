3575b9494432440ace2a916942b573677ba5553e
Arjun J <arjun.jwn@gmail.com>
2025-02-23 19:23:50 +0530

    raw strings for regex pattern

--- generate/generate.py
+++ generate/generate.py
@@ -87,7 +87,7 @@ if __name__ == '__main__':
         # condition = ['O=C(CCc1cn[nH]c1)NCCC1CC2CCC1C2', 'O=C(CCC(=O)NCC1CCCO1)NCc1ccccc1', 'O=S(=O)(Cc1ccon1)NCc1cccs1']  # sim 0.9, 0.8, ~0.7
 
 
-        pattern =  "(\[[^\]]+]|<|Br?|Cl?|N|O|S|P|F|I|b|c|n|o|s|p|\(|\)|\.|=|#|-|\+|\\\\|\/|:|~|@|\?|>|\*|\$|\%[0-9]{2}|[0-9])"
+        pattern =  r"(\[[^\]]+]|<|Br?|Cl?|N|O|S|P|F|I|b|c|n|o|s|p|\(|\)|\.|=|#|-|\+|\\\\|\/|:|~|@|\?|>|\*|\$|\%[0-9]{2}|[0-9])"
         regex = re.compile(pattern)
         #lens = [len(regex.findall(i)) for i in smiles]
         #max_len = max(lens)
